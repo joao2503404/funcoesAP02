b2e8a52f99dc050594285819b5b3eb6fcc999e93
joaovieira0087 <joaovieira0087@gmail.com>
2026-04-25 12:25:22 -0300

    exe 04

---
+++ exe04funcoes.py
@@ -0,0 +1,8 @@
+def verificar_par(numero):
+     if numero % 0:
+          return True
+     else:
+        return False
+
+Numero = int(input("Digite o numero:"))
+Resultado = verificar_par(Numero)
