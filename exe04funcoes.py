4f016b154cd6df2d93f2975447e145eaeb3484ef
joaovieira0087 <joaovieira0087@gmail.com>
2026-04-25 13:09:52 -0300

    Exercício 04 de Funções
    Co-authored-by: joaovieira0087 <joaovieira0087@gmail.com>

--- exe04funcoes.py
+++ exe04funcoes.py
@@ -1,8 +1,9 @@
 def verificar_par(numero):
-     if numero % 0:
+     if numero % 2 == 0:
           return True
      else:
         return False
 
 Numero = int(input("Digite o numero:"))
 Resultado = verificar_par(Numero)
+print(Resultado)
